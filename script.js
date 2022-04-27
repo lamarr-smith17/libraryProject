6f6ea67adea8eba28c61a3ea676d9538b823af40
LaMarr Smith <ljsmith19@my.trine.edu>
2022-04-27 12:26:53 -0400

    Trying to figure out how to access A property from the arrary in the for loop

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ class Book{
     addBook(){
         return `Title: ${this.title} \n Author: ${this.author} \n Pages: ${this.numOfPages} \n Read Status: ${this.bookStatus}`;
     }
+
 }
 
 let eightySix = new Book('86: Eighty-Six', 'Asato Asato', '326', 'Read');
@@ -21,12 +22,14 @@ let grimgar = new Book('Grimgar', 'Ao', '326', 'Read');
 let tensei = new Book('Mushoku Tensei', 'Rudeus', '326', 'Unread');
 let myLibrary = [eightySix.addBook(), grimgar.addBook(), tensei.addBook()];
 for (let i = 0; i < myLibrary.length; i++){
-    let newDiv = document.createElement('div');
-    let newP = document.createElement('p');
-    mainContainer.appendChild(newDiv);
-    newDiv.appendChild(newP);
-    newDiv.classList.add('class' + i)
-    newP.innerHTML = myLibrary[i]
+    let bookEntry = document.createElement('div');
+    let titleText = document.createElement('p');
+    mainContainer.appendChild(bookEntry);
+    bookEntry.appendChild(titleText);
+    bookEntry.setAttribute("id", "div" + i);
+    bookEntry.setAttribute("class", "bookHolder");
+    titleText.innerText = myLibrary[i]
+    console.log(myLibrary[i].title)
 }
 
-
+console.log(eightySix.title)
