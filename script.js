8f4c781efc582452c4103002732e6d0f4fef1bba
LaMarr Smith <ljsmith19@my.trine.edu>
2022-04-28 22:11:54 -0400

    Added form popup/close functionality Going to be adding the delete book function next

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-const pTest = document.getElementById('test');
 const mainContainer = document.getElementById('main-container');
+const addBtn = document.getElementById('addBtn');
+const closeBtn = document.getElementById('closeBtn');
+const formHolder = document.getElementById('formHolder')
 const b = document.getElementById('button');
-const bookEntry = document.createElement('div');
-const titleText = document.createElement('p');
-const authorText = document.createElement('p');
-const pageText = document.createElement('p');
-const readText = document.createElement('p');
-mainContainer.appendChild(bookEntry);
-bookEntry.append(titleText, authorText, pageText, readText);
+
+
 
 // Book array and book class
 class Book{
@@ -20,7 +17,7 @@ class Book{
 }
 let eight = new Book('86: Eighty-Six', 'Asato Asato', '326', 'Read');
 
-let myLibrary = [eight];
+let myLibrary = [];
 
 
 // Add book function that pushes the book to the array and creates the new book object
@@ -32,29 +29,44 @@ function addBook (ev){
 
     // Each time a new book is added, this clears the HTML so a book does not repeat it's entry
     mainContainer.innerHTML = ""; 
-
-    bookCardCreater();
     
+    bookCardCreater();
+    formClose();
     
 }
 // Creates the new book card which displays the title, author, page count, and read status
 function bookCardCreater(){
     for (let i = 0; i < myLibrary.length; i++){
-        
+        const bookEntry = document.createElement('div');
+        const titleText = document.createElement('p');
+        const authorText = document.createElement('p');
+        const pageText = document.createElement('p');
+        const readText = document.createElement('p');
+        mainContainer.appendChild(bookEntry);
+        bookEntry.append(titleText, authorText, pageText, readText);
         bookEntry.setAttribute("id", "div" + i);
         bookEntry.setAttribute("class", "bookHolder");
         titleText.innerText = myLibrary[i].title;
         authorText.innerText = myLibrary[i].author;
         pageText.innerText = myLibrary[i].numOfPages;
         readText.innerText = myLibrary[i].bookStatus;
-    
     }
 }
 
-bookCardCreater()
+function deleteBook(){
+    
+}
+
+function formPopup (){
+    formHolder.style.display = "block";
+}
+function formClose (){
+    formHolder.style.display = "none";
+}
+
 b.addEventListener('click', addBook);
-
-
+addBtn.addEventListener('click', formPopup);
+closeBtn.addEventListener('click', formClose);
 
 
 
